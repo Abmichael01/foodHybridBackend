1f53d8a00fc685b9d01f96dcf68a826215896140
O-GDev <omiwoleoluwagbogo@gmail.com>
2025-08-04 11:52:24 +0100

    commit

--- users/models.py
+++ users/models.py
@@ -112,6 +112,7 @@ class OrderDeliveryConfirmation(models.Model):
     
     is_confirmed = models.BooleanField(default=False)
     confirmed_at = models.DateTimeField(null=True, blank=True)
+    
     created_at = models.DateTimeField(auto_now_add=True)
 
     @property
