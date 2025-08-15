da17cf24687a0feeab29ce0b9d87ae8bab469211
O-GDev <omiwoleoluwagbogo@gmail.com>
2025-08-15 09:02:48 +0100

    update

--- foodhybrid/settings.py
+++ foodhybrid/settings.py
@@ -129,7 +129,9 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
       'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,  # keeps connections open for performance
+        ssl_require=True
     )
 }
 
