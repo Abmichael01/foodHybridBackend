a8cf86b2376b8f0e43556386e739dff1bff5f82c
O-GDev <105860521+O-GDev@users.noreply.github.com>
2025-08-23 22:45:56 +0100

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,4 @@
-tofrom rest_framework import serializers
+from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError as DjangoValidationError
 
