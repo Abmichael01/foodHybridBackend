516e1bf1e81b831039506720cb267bc2d4ee8795
O-GDev <omiwoleoluwagbogo@gmail.com>
2025-08-23 04:18:37 +0100

    update

--- foodhybrid/urls.py
+++ foodhybrid/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for foodhybrid project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
@@ -21,41 +5,35 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
-from users import views as user_views
-
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="FoodHybrid API",
       default_version='v1',
-      description="Test description",
+      description="FoodHybrid API documentation",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="admin@foodhybrid.com"),
-      license=openapi.License(name="foodhybrid License"),
+      license=openapi.License(name="FoodHybrid License"),
    ),
    public=True,
-   permission_classes=(permissions.AllowAny,),
+   permission_classes=(permissions.AllowAny,),  # public access
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  
-    path('api/', include([
-        path('users/', include('users.urls')),
-        path('wallet/', include('wallet.urls')),
-        path('shops/', include('shop.urls')),
-        path('cart/', include('cart.urls')),
-    ])),
-    # path('users/', include("users.urls")),
-    # path('wallet/', include('wallet.urls')),
-    # path('shops/', include('shop.urls')),
-    # path('cart/', include('cart.urls')),
-    # path('auth/', include("dj_rest_auth.urls")),
-    # path("auth/registration/", include("dj_rest_auth.registration.urls")),
+    path('admin/', admin.site.urls),
+    
+    # API routes
+    path('api/users/', include('users.urls')),
+    path('api/wallet/', include('wallet.urls')),
+    path('api/shops/', include('shop.urls')),
+    path('api/cart/', include('cart.urls')),
+
+    # Swagger / Redoc
     path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-#    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
+# Serve media files in debug
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
