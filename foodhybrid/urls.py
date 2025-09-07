1372ee67218257e263943d27ec7f45783d70fc8b
O-GDev <105860521+O-GDev@users.noreply.github.com>
2025-09-07 06:40:52 +0100

    Update urls.py

--- foodhybrid/urls.py
+++ foodhybrid/urls.py
@@ -5,6 +5,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 schema_view = get_schema_view(
    openapi.Info(
